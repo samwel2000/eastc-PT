32c0794c63b7368dbeced958e66549ff56d6032e
samwel2000 <samwelgfrey@gmail.com>
2021-04-01 14:21:27 -0500

    Update settings.py

--- eastc/settings.py
+++ eastc/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dyiq^hu2&hwnxhbxo@k6^uvpf$&jn*eme8o&kx8ff2x5)0lk7p'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
